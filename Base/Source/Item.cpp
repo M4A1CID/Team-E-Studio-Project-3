ea10482555559b113b1a72a8daf2710dd0446bb2
M4A1CID <konata_lzumi@hotmail.com>
2015-08-17 11:59:46 +0800

    Modified Item class to contain various functions

--- Base/Source/Item.cpp
+++ Base/Source/Item.cpp
@@ -9,3 +9,72 @@ CItem::~CItem(void)
 {
 
 }
+
+//Set the position of this Item
+void CItem::setPosition(Vector3 Pos)
+{
+	this->Pos = Pos;
+}
+void CItem::setPosition_X(float x)
+{
+	this->Pos.x = x;
+}
+void CItem::setPosition_Y(float y)
+{
+	this->Pos.y = y;
+}
+void CItem::setPosition_Z(float z)
+{
+	this->Pos.z = z;
+}
+
+//Get the position of this item
+Vector3 CItem::getPosition(void)
+{
+	return this->Pos;
+}
+
+// Set the scale of this item
+void CItem::setScale(Vector3 Scale)
+{
+	this->Scale = Scale;
+}
+void CItem::setScale_X(float x)
+{
+	this->Scale.x = x;
+}
+void CItem::setScale_Y(float y)
+{
+	this->Scale.y = y;
+}
+void CItem::setScale_Z(float z)
+{
+	this->Scale.z = z;
+}
+// Get the scale of this item
+Vector3 CItem::getScale(void)
+{
+	return this->Scale;
+}
+
+// Set the active of this item
+void CItem::setActive(bool active)
+{
+	this->active = active;
+}
+// Get the active of this item
+bool CItem::getActive(void)
+{
+	return this->active;
+}
+
+// Set the geoType of this item
+void CItem::setGeoType(int geoType)
+{
+	this->geoType = geoType;
+}
+// Get the geoType of this item
+int CItem::getGeoType(void)
+{
+	return this->geoType;
+}
--- Base/Source/Item.h
+++ Base/Source/Item.h
@@ -1,10 +1,42 @@
 #pragma once
 
+#include"Vector3.h"
+
 class CItem
 {
 public:
 	CItem(void);
 	virtual~CItem(void);
 
+	//Set the position of this item
+	void setPosition(Vector3);
+	void setPosition_X(float);
+	void setPosition_Y(float);
+	void setPosition_Z(float);
+
+	//Get the position of this item
+	Vector3 getPosition(void);
+	
+	// Set the scale of this item
+	void setScale(Vector3);
+	void setScale_X(float);
+	void setScale_Y(float);
+	void setScale_Z(float);
+	// Get the scale of this item
+	Vector3 getScale(void);
+
+	// Set the active of this item
+	void setActive(bool);
+	// Get the active of this obj
+	bool getActive(void);
+
+	// Set the geoType of this item
+	void setGeoType(int);
+	// Get the geoType of this item
+	int getGeoType(void);
 private:
+	Vector3 Pos;		// Item's position
+	Vector3 Scale;		// Item's scale
+	bool active;		// Item's active
+	int geoType;		// Item's Geometric type
 };
