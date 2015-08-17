2c0d16fbd3be9a763b608a26a2f8af59526bb1af
M4A1CID <konata_lzumi@hotmail.com>
2015-08-17 10:44:01 +0800

    Added basic item class Added basic Physics class

---
+++ Base/Source/Item.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class CItem
+{
+public:
+	CItem(void);
+	virtual~CItem(void);
+
+private:
+};
---
+++ Base/Source/Physics.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class CPhysics
+{
+	CPhysics(void);
+	~CPhysics(void);
+
+private:
+
+};
