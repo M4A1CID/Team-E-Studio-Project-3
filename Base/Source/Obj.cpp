98a8f7657b185f1ac7c633b58f504c97ebfd26bf
TohYiFunTroy <c3po.toh@gmail.com>
2015-08-17 11:37:59 +0800

    Modified OBJ files

--- Base/Source/Obj.cpp
+++ Base/Source/Obj.cpp
@@ -3,6 +3,11 @@
 
 CObj::CObj(void)
 {
+	Pos.x = 0;
+	Pos.y = 0;
+	Pos.z = 0;
+	active = false;
+	geoType = 8;
 }
 
 
--- Base/Source/Obj.h
+++ Base/Source/Obj.h
@@ -1,8 +1,15 @@
 #pragma once
+#include "Vector3.h"
+#include "Mesh.h"
+
 class CObj
 {
 public:
 	CObj(void);
 	virtual ~CObj(void);
+
+	Vector3 Pos;
+	bool active;
+	int geoType;
 };
 
